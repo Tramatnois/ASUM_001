57f7e13ed9d02eb8a7f396892569c5f68d347b75
tramatnois <tramatnois@192.168.0.31>
2017-07-22 23:05:11 +0200

    init

--- asum.regalpruefung/src/mvc/main/main.java
+++ asum.regalpruefung/src/mvc/main/main.java
@@ -3,14 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package asum.regalpruefung;
+package mvc.main;
 
 /**
  *
  * @author tramatnois
  */
-public class AsumRegalpruefung {
-
-
+public class main {
+    
+    public static void main(String[] args) {
+        
+    }
     
 }
