d00e96c1a4558cf95088d3f6885b00cde167e7e5
danie <danie@192.168.0.234>
2017-07-23 12:11:56 +0200

    Merge origin/master
    Conflicts:
    	asum.regalpruefung/config.properties

---
+++ asum.regalpruefung/src/mvc/model/CustomerDTO.java
@@ -0,0 +1,99 @@
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.model;
+
+/**
+ *
+ * @author LT Dan
+ */
+public class CustomerDTO {
+
+    int idcustomer;
+    private String name;
+    private String street;
+    private String zipcode;
+    private String city;
+    private String contactperson;
+    private String phone;
+    private String fax;
+    private String email;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getIdcustomer() {
+        return idcustomer;
+    }
+
+    public void setIdcustomer(int idcustomer) {
+        this.idcustomer = idcustomer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getContactperson() {
+        return contactperson;
+    }
+
+    public void setContactperson(String contactperson) {
+        this.contactperson = contactperson;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
